90e8d5d704e314c868c288bf2ea09ff6c0f0c8bd
Caio <caiomorato94@hotmail.com>
2018-05-23 13:01:30 -0300

    Rodapé

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
-  <div id="app">
+  <span>
+
+    <header>
       <nav-bar logo="Social" url="#" cor ="green darken-1" />
-    <router-view/>
-  </div>
+    </header>
+
+    <main>
+      <div class="container">
+
+        <div class="row">
+
+          <grid-vue tamanho="4">
+            Menu
+          </grid-vue>
+
+          <grid-vue tamanho="8">
+            <router-view/>
+          </grid-vue>
+            
+        </div>
+      </div>
+    </main>
+
+    <footer-vue cor="green darken-1" logo="Social" descricao="Teste de descrição" ano="2018">
+
+      <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+
+    </footer-vue>
+
+     
+  </span>
 </template>
 
 <script>
 
 import NavBar from '@/components/layouts/NavBar'
+import FooterVue from '@/components/layouts/FooterVue'
+import GridVue from '@/components/layouts/GridVue'
 
 export default {
   name: 'App',
   components:{
-    NavBar
+    NavBar,
+    FooterVue,
+    GridVue
   }
 }
 </script>
