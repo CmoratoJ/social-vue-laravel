f91c83928b5c1ee1192e7fe7d1e2e4f3507b189f
Caio <caiomorato94@hotmail.com>
2018-05-23 12:01:35 -0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    
+      <nav-bar logo="Social" url="#" cor ="green darken-1" />
     <router-view/>
   </div>
 </template>
 
 <script>
+
+import NavBar from '@/components/layouts/NavBar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    NavBar
+  }
 }
 </script>
 
